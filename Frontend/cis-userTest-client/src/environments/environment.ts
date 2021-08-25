4e81e8267488627215be9aefed0fc95c79868ceb
SergeyMaleev <72796049+SergeyMaleev@users.noreply.github.com>
2021-08-25 15:23:12 +0400

    исправлен путь к серверу

--- Frontend/cis-userTest-client/src/environments/environment.ts
+++ Frontend/cis-userTest-client/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:50001',
-  allowedRoutes: ['localhost:50001'],
-  disallowedRoutes: ['localhost:50001']
+  apiUrl: 'https://localhost:5001',
+  allowedRoutes: ['localhost:5001'],
+  disallowedRoutes: ['localhost:5001']
 };
 
 /*
