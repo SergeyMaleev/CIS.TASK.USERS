5c0a9f9d30e1a55cb69c025759514d37a967e67d
SergeyMaleev <72796049+SergeyMaleev@users.noreply.github.com>
2021-09-08 10:24:10 +0400

    Добавлен проект с тестами

---
+++ Backend/Api.Tests/UserServicesTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+namespace Api.Tests
+{
+    public class Tests
+    {
+        public Tests()
+        {
+
+        }
+
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+            Assert.Pass();
+        }
+    }
+}
